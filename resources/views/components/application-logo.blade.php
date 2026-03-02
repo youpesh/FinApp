6b6aea31f591a8741b53947f3a9912c1b1c0aeb3
youpesh <25112850+youpesh@users.noreply.github.com>
2026-03-01 23:53:29 -0500

    fix: prevent logo layout shift by adding explicit dimensions

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
-<img src="{{ asset('images/logo.png') }}" {{ $attributes->merge(['class' => 'h-12 w-auto']) }}
+<img src="{{ asset('images/logo.png') }}" width="640" height="640" {{ $attributes->merge(['class' => 'h-12 w-auto']) }}
     alt="{{ config('app.name', 'Smart Finance') }}">
